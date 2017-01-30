cff2402a4f962526ced79acacbe8bd5552d552ac
ddubuc <ddubuc@aneo.fr>
2017-01-30 23:48:52 +0100

    Issue #1 fix reference header

--- Hipe/scripts/headerList.sh
+++ Hipe/scripts/headerList.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find \( -name '*.h' -o -name '*.hpp' \) -type f -exec sh -c 'echo "#include <"{}">" | sed "s/\.\///g"' \;
+find \( -name '*.h' -o -name '*.hpp' \) -type f -exec sh -c 'echo "#include <filter/Algos/"{}">" | sed "s/\.\///g"' \;
 
 exit 0
