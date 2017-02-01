4bc1c38e481b10af8d626761b4840ace15cb3a6a
ddubuc <ddubuc@aneo.fr>
2017-02-01 17:41:22 +0100

    Fix script to generate reference

--- Hipe/scripts/headerList.sh
+++ Hipe/scripts/headerList.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 prefix=$1
-find \( -name '*.h' -o -name '*.hpp' \) -type f -exec sh -c 'echo "#include <$1/"{}">" | sed "s/\.\///g"' \;
+find \( -name '*.h' -o -name '*.hpp' \) -type f -exec sh -c "echo \"#include <${prefix}/"{}">\" | sed \"s/\.\///g\"" \;
 
 exit 0
