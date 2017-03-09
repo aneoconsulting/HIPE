30cbdbb6d4d6bd877a8894c7c8319598c65e5837
ddubuc <ddubuc@aneo.fr>
2017-03-09 17:11:29 +0100

    #9 bug fix cmake path for linux

--- Hipe/OwnerConfigPath.cmake
+++ Hipe/OwnerConfigPath.cmake
@@ -19,6 +19,7 @@ else()
 SET(HIPE_PLATFORM "linux64")
 endif()
 
+if (WIN32)
 SET(Boost_DIR "${Hipe_external}/${HIPE_PLATFORM}/boost_1_62_0/"   CACHE PATH "Boost_DIR" FORCE )
 SET(Boost_INCLUDE_DIR "${Boost_DIR}"  CACHE PATH "Boost_INCLUDE_DIR" FORCE )
 SET(BOOST_LIBRARYDIR "${Boost_DIR}/lib64-msvc-12.0"  CACHE PATH "BOOST_LIBRARYDIR" FORCE )
@@ -30,3 +31,16 @@ SET(x264_DIR "${Hipe_external}/${HIPE_PLATFORM}/x264-devel-${HIPE_PLATFORM}/" CA
 SET(LIVE_DIR "${Hipe_external}/${HIPE_PLATFORM}/live555/install"  CACHE PATH "Live_DIR" FORCE )
 
 SET(FFMPEG_DIR "${Hipe_external}/${HIPE_PLATFORM}/ffmpeg"  CACHE PATH "FFMPEG_LIBRARYDIR" FORCE )
+else()
+SET(Boost_DIR "${Hipe_external}/${HIPE_PLATFORM}"   CACHE PATH "Boost_DIR" FORCE )
+SET(Boost_INCLUDE_DIR "${Boost_DIR}/include"  CACHE PATH "Boost_INCLUDE_DIR" FORCE )
+SET(BOOST_LIBRARYDIR "${Boost_DIR}/lib/"  CACHE PATH "BOOST_LIBRARYDIR" FORCE )
+
+SET(OpenCV_DIR "${Hipe_external}/${HIPE_PLATFORM}/"  CACHE PATH "OpenCV Directory" FORCE )
+
+SET(x264_DIR "${Hipe_external}/${HIPE_PLATFORM}/" CACHE PATH "x264" FORCE )
+
+SET(LIVE_DIR "${Hipe_external}/${HIPE_PLATFORM}/"  CACHE PATH "Live_DIR" FORCE )
+
+SET(FFMPEG_DIR "${Hipe_external}/${HIPE_PLATFORM}/"  CACHE PATH "FFMPEG_LIBRARYDIR" FORCE )
+endif()
