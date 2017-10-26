bef70ad88abf429aceff7eab081fc77f3a9c0b88
szaidi <szaidi@aneo.fr>
2017-10-26 14:36:00 +0200

    #65 add  ComputeRegularGrid (@jrye implementation)

---
+++ Hipe/header/filter/algos/ComputeRegularGrid.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <filter/tools/RegisterClass.h>
+#include <filter/IFilter.h>
+#include <core/HipeStatus.h>
+
+#include <data/ImageData.h>
+#include <data/ImageArrayData.h>
+#include "data/ShapeData.h"
+
+namespace filter
+{
+	namespace algos
+	{
+		class ComputeRegularGridFilter : public filter::IFilter
+		{
+			CONNECTOR(data::ImageData, data::ShapeData);
+
+			REGISTER(ComputeRegularGridFilter, ()), _connexData(data::INDATA)
+			{
+				rows = 4;
+				cols = 4;
+				width = 800;
+				height = 800;
+			}
+
+			REGISTER_P(unsigned int, rows);
+			REGISTER_P(unsigned int, cols);
+			REGISTER_P(float, width);
+			REGISTER_P(float, height);
+
+			HipeStatus process() override;
+
+			/*!
+			@brief Calculate the position of the points along a regular grid.
+			@param grid_size The size of the grid (rows * columns).
+			@return A vector of Point2f objects representing the points of the regular grid, orderd by row then column.
+			*/
+			std::vector<cv::Point2f> compute_regular_grid(unsigned int rows, unsigned int cols, float width, float height, bool corners_only = false);
+		};
+
+		ADD_CLASS(ComputeRegularGridFilter, rows, cols, width, height);
+	}
+}
---
+++ Hipe/source/filter/algos/ComputeRegularGrid.cpp
@@ -0,0 +1,64 @@
+
+#include <core/HipeStatus.h>
+#include <ComputeRegularGrid.h>
+namespace filter {
+	namespace algos {
+		HipeStatus ComputeRegularGrid::process()
+		{
+			data::ImageData data = _connexData.pop();
+			cv::Mat& image = data.Array()[0];
+
+			if (!image.data) throw HipeException("Error - filter::algos::ComputeRegularGridFilter::process - no input data found.");
+
+			std::vector<cv::Point2f> grid_points = compute_regular_grid(rows, cols, width, height);
+
+			data::ShapeData output;
+			output << grid_points;
+
+			_connexData.push(output);
+
+			return OK;
+		}
+
+
+		std::vector<cv::Point2f> ComputeRegularGrid::compute_regular_grid(unsigned int rows, unsigned int cols, float width, float height, bool corners_only = false)
+		{
+			/*
+			--0----0----0--
+			= n * --0--
+			= n * ----
+			*/
+			float x_spacing = width / cols,
+				y_spacing = height / rows,
+				x_margin = x_spacing / 2,
+				y_margin = y_spacing / 2;
+
+			if (corners_only)
+			{
+				// x_margin is equivalent to "x_min", same for y.
+				float x_max = width - x_margin, y_max = height - y_margin;
+				return std::vector<cv::Point2f> {
+						cv::Point2f{ x_margin, y_margin },
+						cv::Point2f{ x_max, y_margin },
+						cv::Point2f{ x_margin, y_max },
+						cv::Point2f{ x_max, y_max }
+				};
+			}
+
+			std::vector<cv::Point2f> grid_points(rows * cols);
+
+			for (unsigned int j = 0; j < rows; ++j)
+			{
+				float y = y_margin + j * y_spacing;
+				for (unsigned int i = 0; i < cols; ++i)
+				{
+					float x = x_margin + i * x_spacing;
+					int k = j * cols + i;
+					grid_points[k].x = x;
+					grid_points[k].y = y;
+				}
+			}
+			return grid_points;
+		}
+	}
+}
