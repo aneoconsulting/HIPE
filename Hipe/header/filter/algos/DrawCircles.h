aa1a926f97cd2fb1556cba18b8f03dded6d97ddb
lostanlen <mlostanlen@aneo.fr>
2017-10-26 14:21:53 +0200

    #64 DrawCircles Filter

---
+++ Hipe/header/filter/algos/DrawCircles.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <filter/tools/RegisterClass.h>
+#include <filter/IFilter.h>
+#include <core/HipeStatus.h>
+#include "data/ImageArrayData.h"
+#include <vector>
+#include "data/ImageData.h"
+
+
+namespace filter
+{
+	namespace algos
+	{
+
+		class DrawCircles : public filter::IFilter
+		{
+
+			//! @brief Scale factor for fonts when drawing text on images.
+			static constexpr float FACTOR_FONT_SCALE = 0.004f;
+
+			//data::ConnexData<data::ImageArrayData, data::ImageArrayData> _connexData;
+			CONNECTOR(data::Data, data::ImageData);
+
+			REGISTER(DrawCircles, ()), _connexData(data::INDATA)
+			{
+				thickness = 5;
+				center_radius = 3;
+				circle_colorR = 0;
+				circle_colorG = 255;
+				circle_colorB = 0;
+				center_colorR = 0;
+				center_colorG = 0;
+				center_colorB = 255;
+				text_colorR = 255;
+				text_colorG = 0;
+				text_colorB = 0;
+				
+				
+				factor_font_scale = FACTOR_FONT_SCALE;
+			}
+
+			REGISTER_P(int, thickness);
+			REGISTER_P(int, center_radius);
+			REGISTER_P(int, circle_colorR);
+			REGISTER_P(int, circle_colorG);
+			REGISTER_P(int, circle_colorB);
+			REGISTER_P(int, center_colorR);
+			REGISTER_P(int, center_colorG);
+			REGISTER_P(int, center_colorB);
+			REGISTER_P(int, text_colorR);
+			REGISTER_P(int, text_colorG);
+			REGISTER_P(int, text_colorB);
+			REGISTER_P(float, factor_font_scale);
+
+
+
+		public:
+			HipeStatus DrawCircles::process() override;
+			
+			};
+
+			ADD_CLASS(DrawCircles, thickness, center_radius, circle_colorR, circle_colorG, circle_colorB, center_colorR, center_colorG, center_colorB, text_colorR, text_colorG, text_colorB, factor_font_scale);
+		}
+	}
---
+++ Hipe/source/filter/algos/DrawCircles.cpp
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "filter/algos/DrawCircles.h"
+#include "data/ShapeData.h"
+#include "data/ImageData.h"
+
+
+namespace filter
+{
+	namespace algos
+	{
+
+		HipeStatus DrawCircles::process()
+		{
+			
+
+			while (!_connexData.empty()) // While i've parent data
+			{
+
+				data::ImageData images;
+				data::ShapeData circles;
+
+				data::Data input = _connexData.pop();
+				if(input.getType()== data::IMGF)
+				{
+					 images = input;
+				}
+				else circles = input; 
+
+				data::Data input2 = _connexData.pop();
+
+				if (input2.getType() == data::IMGF)
+				{
+					 images = input2;
+					 
+				}
+				else  circles = input2; 
+			
+				float const font_scale = factor_font_scale * std::min(images.getMat().rows, images.getMat().cols);
+				SIZE_T const s = circles.CirclesArray().size();
+				cv::Scalar const circle_color(circle_colorR, circle_colorG, circle_colorB);
+				cv::Scalar const center_color(center_colorR, center_colorG, center_colorB);
+				cv::Scalar const text_color(text_colorR, text_colorG, text_colorB);
+			
+						
+					for (int i (0); i < s ; ++i)
+					{
+
+						cv::Point const center(cvRound(circles.CirclesArray()[i][0]), cvRound(circles.CirclesArray()[i][1]));
+						int  const radius = cvRound(circles.CirclesArray()[i][2]);
+						// Draw perimeter.
+						circle(images.getMat(), center, radius, circle_color, thickness, 8, 0);
+						// Draw center.
+						circle(images.getMat(), center, center_radius, center_color, -thickness, 8, 0);
+						putText(images.getMat(), std::to_string(i), center, CV_FONT_HERSHEY_PLAIN, font_scale, text_color, 2);
+					}
+					_connexData.push(images);
+			}
+			return OK;
+		}
+	}
+}
