c6a206f002046c90f320df525c657d0d73f19789
tmirre <tmirre@aneo.fr>
2017-11-07 16:42:31 +0100

    #69 Fixed inverted objects in Homography filter

--- Hipe/header/filter/algos/Homography.h
+++ Hipe/header/filter/algos/Homography.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <filter/tools/RegisterTools.hpp>
+#include <filter/tools/RegisterClass.h>
 #include <filter/IFilter.h>
 #include <core/HipeStatus.h>
 #include <opencv2/highgui/highgui.hpp>
@@ -10,7 +10,7 @@ namespace filter
 {
 	namespace algos
 	{
-		
+
 		class Homography : public IFilter
 		{
 
@@ -19,7 +19,7 @@ namespace filter
 
 			REGISTER(Homography, ()), _connexData(data::INDATA)
 			{
-				
+
 			}
 			REGISTER_P(int, unused);
 
@@ -33,35 +33,60 @@ namespace filter
 			HipeStatus process()
 			{
 				data::MatcherData md = _connexData.pop();
+
+				if (md.goodMatches().empty())
+				{
+					_connexData.push(data::ShapeData());
+					return OK;
+				}
+
 				std::vector<cv::Point2f> obj;
 				std::vector<cv::Point2f> scene;
-				for (size_t i = 0; i < md.GoodMatches().size(); i++)
+
+				for (size_t i = 0; i < md.goodMatches().size(); i++)
 				{
 					//-- Get the keypoints from the good matches
-					obj.push_back(md.Inliers2()[md.GoodMatches()[i].queryIdx].pt);
-					scene.push_back(md.Inliers1()[md.GoodMatches()[i].trainIdx].pt);
+					obj.push_back(md.inliers1()[md.goodMatches()[i].queryIdx].pt);
+					scene.push_back(md.inliers2()[md.goodMatches()[i].queryIdx].pt);
 				}
-				cv::Mat H = findHomography(obj, scene, cv::RANSAC);
-				//-- Get the corners from the image_1 ( the object to be "detected" )
-				std::vector<cv::Point2f> obj_corners(4);
-				obj_corners[0] = cvPoint(0, 0); obj_corners[1] = cvPoint(md.RequestImage().cols, 0);
-				obj_corners[2] = cvPoint(md.RequestImage().cols, md.RequestImage().rows); obj_corners[3] = cvPoint(0, md.RequestImage().rows);
-				std::vector<cv::Point2f> scene_corners(4);
-				perspectiveTransform(obj_corners, scene_corners, H);
-				//-- Draw lines between the corners (the mapped object in the scene - image_2 )
-				//TODO REMOVE AFTER TEST
-				line(md.GoodMatches(), scene_corners[0] + cv::Point2f(md.RequestImage().cols, 0), scene_corners[1] + cv::Point2f(md.RequestImage().cols, 0), cv::Scalar(0, 255, 0), 4);
-				line(md.GoodMatches(), scene_corners[1] + cv::Point2f(md.RequestImage().cols, 0), scene_corners[2] + cv::Point2f(md.RequestImage().cols, 0), cv::Scalar(0, 255, 0), 4);
-				line(md.GoodMatches(), scene_corners[2] + cv::Point2f(md.RequestImage().cols, 0), scene_corners[3] + cv::Point2f(md.RequestImage().cols, 0), cv::Scalar(0, 255, 0), 4);
-				line(md.GoodMatches(), scene_corners[3] + cv::Point2f(md.RequestImage().cols, 0), scene_corners[0] + cv::Point2f(md.RequestImage().cols, 0), cv::Scalar(0, 255, 0), 4);
-				data::ShapeData res;
-				std::vector<cv::Point2f> corners;// = std::array<cv::Point2f, 4>(scene_corners[0], scene_corners[1], scene_corners[2], scene_corners[3]);
-				corners.push_back(scene_corners[0]);
-				corners.push_back(scene_corners[1]);
-				corners.push_back(scene_corners[2]);
-				corners.push_back(scene_corners[3]);
-				res.QuadrilatereArray().push_back(corners);
-				_connexData.push(res);
+
+				// Note: Some errors occured BEFORE fixing the code, so the try/catch block may not be mandatory, but mind the filter should not kill the whole process when playing a video. 
+				try
+				{
+					cv::Mat H = findHomography(obj, scene, cv::RANSAC);
+
+
+					//-- Get the corners from the image_1 ( the object to be "detected" )
+					std::vector<cv::Point2f> obj_corners(4);
+					obj_corners[0] = cvPoint(0, 0);
+					obj_corners[1] = cvPoint(md.patternImage().cols, 0);
+
+					obj_corners[2] = cvPoint(md.patternImage().cols, md.patternImage().rows);
+					obj_corners[3] = cvPoint(0, md.patternImage().rows);
+
+					std::vector<cv::Point2f> scene_corners(4);
+					perspectiveTransform(obj_corners, scene_corners, H);
+
+					data::ShapeData res;
+					std::vector<cv::Point2f> corners;
+					corners.push_back(scene_corners[0]);
+					corners.push_back(scene_corners[1]);
+					corners.push_back(scene_corners[2]);
+					corners.push_back(scene_corners[3]);
+
+					res.QuadrilatereArray().push_back(corners);
+
+					_connexData.push(res);
+				}
+				catch (const std::exception& e)
+				{
+					std::stringstream errorMessage;
+					errorMessage << "Error in Homography filter: " << e.what();
+					//throw HipeException(errorMessage.str());
+					std::cout << errorMessage.str();
+					_connexData.push(data::ShapeData());
+				}
+
 				return OK;
 			}
 
