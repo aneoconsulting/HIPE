e3107eb2917793801b326c5bc81feedf5b47c87e
tmirre <tmirre@aneo.fr>
2017-11-09 15:46:21 +0100

    #69 Updated homography filter behavior. Now accepts multiple overlay related data to draw on a unique input source image

--- Hipe/header/filter/algos/OverlayFilter.h
+++ Hipe/header/filter/algos/OverlayFilter.h
@@ -5,106 +5,13 @@
 #include <core/HipeStatus.h>
 #include <data/ImageData.h>
 #include <data/ShapeData.h>
+#include <data/PatternData.h>
 
-
-//namespace filter
-//{
-//	namespace algos
-//	{
-//		/**
-//		 * \var OverlayFilter::ratio
-//		 * [TODO]
-//		 */
-//
-//		/**
-//		 * \todo
-//		 * \brief The OverlayFilter filter is used contour regions of interests in an image.
-//		 * 
-//		 * The ConnexData port must contain 2 objects. The image on which the filter will draw, and the list of regions of interest (SquareCrop object).
-//		 */
-//		class OverlayFilter : public filter::IFilter
-//		{
-//			//data::ConnexData<data::ImageArrayData, data::ImageArrayData> _connexData;
-//			CONNECTOR(data::Data, data::ImageData);
-//
-//			REGISTER(OverlayFilter, ()), _connexData(data::INDATA)
-//			{
-//
-//			}
-//
-//			REGISTER_P(double, ratio);
-//
-//			virtual std::string resultAsString() { return std::string("TODO"); };
-//
-//		public:
-//			HipeStatus process()
-//			{
-//				if (_connexData.size() % 2 != 0)
-//				{
-//					throw HipeException("The Overlay missing or text data. Please be sure to link properly with parent");
-//				}
-//
-//				//while (!_connexData.empty()) // While i've parent data
-//				{
-//					data::Data data1 = _connexData.pop();
-//					data::Data data2 = _connexData.pop();
-//
-//					if (data1.getType() != data::IMGF &&
-//						data1.getType() != data::TXT_ARR &&
-//						data1.getType() != data::TXT &&
-//						data1.getType() != data::SQR_CROP)
-//					{
-//						throw HipeException("The Overlay object cant aggregate tan text ATM. Please Develop OverlayFilter");
-//					}
-//					if (data2.getType() != data::IMGF &&
-//						data2.getType() != data::TXT_ARR &&
-//						data2.getType() != data::TXT &&
-//						data2.getType() != data::SQR_CROP)
-//					{
-//						throw HipeException("The Overlay object cant aggregate tan text ATM. Please Develop OverlayFilter");
-//					}
-//					data::ImageData image;
-//					if (data1.getType() != data::IMGF && data2.getType() != data::IMGF)
-//					{
-//						throw HipeException("Missing image to generate overlay text");
-//					}
-//					if (data1.getType() == data::IMGF)
-//						image = static_cast<data::ImageData &>(data1);
-//					if (data2.getType() == data::IMGF)
-//						image = static_cast<data::ImageData &>(data2);
-//
-//					if (data1.getType() == data::TXT || data2.getType() == data::TXT_ARR)
-//						throw HipeException("Text overlay is not yet implemented");
-//
-//					data::SquareCrop crops;
-//					if (data1.getType() == data::SQR_CROP)
-//						crops = static_cast<data::SquareCrop &>(data1);
-//					if (data2.getType() == data::SQR_CROP)
-//						crops = static_cast<data::SquareCrop &>(data2);
-//
-//					for (cv::Rect & rect : crops.getSquareCrop())
-//					{
-//						cv::rectangle(image.getMat(), rect, cv::Scalar(255, 0, 0));
-//					}
-//					_connexData.push(image);
-//				}
-//				return OK;
-//			}
-//		};
-//
-//		ADD_CLASS(OverlayFilter, ratio);
-//	}
-//}
 
 namespace filter
 {
 	namespace algos
 	{
-		/**
-		* \var OverlayFilter::ratio
-		* [TODO]
-		*/
-
 		/**
 		* \todo
 		* \brief The OverlayFilter filter is used contour regions of interests in an image.
@@ -121,120 +28,22 @@ namespace filter
 
 			}
 
-			REGISTER_P(double, ratio);
+			REGISTER_P(char, unused);
 
 			virtual std::string resultAsString() { return std::string("TODO"); };
 
 		public:
-			HipeStatus process()
-			{
-				if (_connexData.size() % 2 != 0)
-				{
-					throw HipeException("The Overlay missing or text data. Please be sure to link properly with parent");
-				}
+			HipeStatus process();
 
-				//while (!_connexData.empty()) // While i've parent data
-				{
-					data::Data data1 = _connexData.pop();
-					data::Data data2 = _connexData.pop();
-					data::ImageData image;
-					data::ShapeData shapes;
+			bool isDrawableSource(const data::Data& data);
+			bool isOverlayData(const data::Data& data);
 
-					// Assert input types are correct
-					if (!assertDataType(data1) || !assertDataType(data2))
-					{
-						throw HipeException("Error in OverlayFilter: The input types mismatch the required ones.");
-					}
+			data::ImageData extractSourceImageData(data::Data& data);
 
-					if (data1.getType() != data::IMGF && data2.getType() != data::IMGF)
-					{
-						throw HipeException("Error in OverlayFilter: Missing image to generate overlay.");
-					}
+			void drawShape(cv::Mat& image, const data::ShapeData& shape);
 
-					// Find image
-					if (data1.getType() == data::IMGF)
-						image = static_cast<data::ImageData &>(data1);
-					else if (data2.getType() == data::IMGF)
-						image = static_cast<data::ImageData &>(data2);
-					else if (data1.getType() == data::IMGF && data2.getType() == data::IMGF)
-						throw HipeException("Error in OverlayFilter: Overlay filter doesn't accept two images in input. Use OverlayMat filter.");
-					else
-						throw HipeException("Error in OverlayFilter: Missing image to generate overlay.");
-
-					// Find overlay data
-					if (data1.getType() == data::SHAPE)
-						shapes = static_cast<data::ShapeData &>(data1);
-					else if (data2.getType() == data::SHAPE)
-						shapes = static_cast<data::ShapeData &>(data2);
-					else
-						throw HipeException("Error in OverlayFilter: Missing shape data to generate overlay.");
-
-					cv::Mat output = image.getMat().clone();
-
-					const cv::Scalar pointsColor(0, 255, 255);
-					const cv::Scalar circlesColor(0, 255, 0);
-					const cv::Scalar rectsColor(255, 0, 0);
-					const cv::Scalar quadsColor(255, 0, 255);
-
-					// Draw points
-					for (const cv::Point2f& point : shapes.PointsArray_const())
-					{
-						cv::circle(output, point, 2, pointsColor, 2);
-					}
-
-					// Draw circles
-					for (const cv::Vec3f& circle : shapes.CirclesArray_const())
-					{
-						cv::Point2f center(circle[0], circle[1]);
-						const float radius = circle[2];
-						cv::circle(output, center, radius, circlesColor, 2);
-					}
-
-					// Draw rects
-					for (const cv::Rect& rect : shapes.RectsArray_const())
-					{
-						cv::rectangle(output, rect, rectsColor, 2);
-					}
-
-					// Draw quads
-					for (const data::four_points& quad : shapes.QuadrilatereArray_const())
-					{
-						for (size_t i = 0; i < quad.size() - 1; ++i)
-						{
-							cv::line(output, quad[i], quad[i + 1], quadsColor, 2);
-						}
-						cv::line(output, quad.front(), quad.back(), quadsColor, 2);
-					}
-
-
-					_connexData.push(data::ImageData(output));
-				}
-				return OK;
-			}
-
-			bool assertDataType(const data::Data& data)
-			{
-				switch (data.getType())
-				{
-				case data::IMGF:
-				case data::SHAPE:
-					return true;
-				case data::TXT_ARR:
-				case data::TXT:
-				{
-					std::stringstream errorMessage;
-					errorMessage << "Error in OverlayFilter: The overlay filter can't display text ATM.\n";
-					errorMessage << "\t found type: " << data::DataTypeMapper::getStringFromType(data.getType());
-					throw HipeException(errorMessage.str());
-				}
-				default:
-					return false;
-				}
-			}
 		};
 
-
-
-		ADD_CLASS(OverlayFilter, ratio);
+		ADD_CLASS(OverlayFilter, unused);
 	}
 }
---
+++ Hipe/source/filter/algos/OverlayFilter.cpp
@@ -0,0 +1,137 @@
+#include <filter/algos/OverlayFilter.h>
+
+HipeStatus filter::algos::OverlayFilter::process()
+{
+	// Assert data is present in connector
+	if (_connexData.empty())
+	{
+		throw HipeException("Error in OverlayFilter: No data in input.");
+	}
+
+	// Separate shapes from source image
+	std::vector<data::Data> overlayData;
+	data::ImageData image;
+	bool isSourceFound = false;
+
+	while (!_connexData.empty())
+	{
+		data::Data data = _connexData.pop();
+		if (isOverlayData(data))
+		{
+			overlayData.push_back(data);
+		}
+		else if (isDrawableSource(data))
+		{
+			if (isSourceFound)
+				throw HipeException("Error in OverlayFilter: Overlay filter only accepts one input source to draw on. Other input data should only be ShapeData objects.");
+			isSourceFound = true;
+
+			image = extractSourceImageData(data);
+		}
+		else
+		{
+			throw HipeException("Error in OverlayFilter: Input type is not handled: " + data.getType());
+		}
+	}
+
+	if (image.empty() || !image.getMat().data)
+		throw HipeException("Error in OverlayFilter: No input image to draw on found.");
+
+	cv::Mat outputImage = image.getMat().clone();
+	for (auto overlayDatum : overlayData)
+	{
+		if (overlayDatum.getType() == data::SHAPE) drawShape(outputImage, static_cast<data::ShapeData &>(overlayDatum));
+	}
+
+	_connexData.push(data::ImageData(outputImage));
+
+	return OK;
+}
+
+bool filter::algos::OverlayFilter::isDrawableSource(const data::Data& data)
+{
+	switch (data.getType())
+	{
+	case data::IMGF:
+	case data::PATTERN:
+		return true;
+	default:
+		return false;
+	}
+}
+
+bool filter::algos::OverlayFilter::isOverlayData(const data::Data& data)
+{
+	switch (data.getType())
+	{
+	case data::SHAPE:
+		return true;
+	case data::TXT:
+	case data::TXT_ARR:
+	{
+		std::stringstream errorMessage;
+		errorMessage << "Error in OverlayFilter: The overlay filter can't display text ATM.\n";
+		errorMessage << "\t found type: " << data::DataTypeMapper::getStringFromType(data.getType());
+		throw HipeException(errorMessage.str());
+	}
+	default:
+		return false;
+	}
+}
+
+filter::data::ImageData filter::algos::OverlayFilter::extractSourceImageData(data::Data& data)
+{
+	const data::IODataType& type = data.getType();
+	if (type == data::IMGF)
+	{
+		return static_cast<data::ImageData &>(data);
+	}
+	else if (type == data::PATTERN)
+	{
+		data::PatternData pattern = static_cast<data::PatternData &>(data);
+		return pattern.imageRequest();
+	}
+	else
+	{
+		return data::ImageData();
+	}
+}
+
+void filter::algos::OverlayFilter::drawShape(cv::Mat& image, const data::ShapeData& shape)
+{
+	const cv::Scalar pointsColor(0, 255, 255);
+	const cv::Scalar circlesColor(0, 255, 0);
+	const cv::Scalar rectsColor(255, 0, 0);
+	const cv::Scalar quadsColor(255, 0, 255);
+
+	// Draw points
+	for (const cv::Point2f& point : shape.PointsArray_const())
+	{
+		cv::circle(image, point, 2, pointsColor, 2);
+	}
+
+	// Draw circles
+	for (const cv::Vec3f& circle : shape.CirclesArray_const())
+	{
+		cv::Point2f center(circle[0], circle[1]);
+		const float radius = circle[2];
+		cv::circle(image, center, radius, circlesColor, 2);
+	}
+
+	// Draw rects
+	for (const cv::Rect& rect : shape.RectsArray_const())
+	{
+		cv::rectangle(image, rect, rectsColor, 2);
+	}
+
+	// Draw quads
+	for (const data::four_points& quad : shape.QuadrilatereArray_const())
+	{
+		for (size_t i = 0; i < quad.size() - 1; ++i)
+		{
+			cv::line(image, quad[i], quad[i + 1], quadsColor, 2);
+		}
+
+		cv::line(image, quad.front(), quad.back(), quadsColor, 2);
+	}
+}
