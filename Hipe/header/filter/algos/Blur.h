f896189764a400a974a7779c96cdcf10442830ac
lostanlen <mlostanlen@aneo.fr>
2017-11-14 11:20:38 +0100

    Camel Case on kernelSize in Blur.h

--- Hipe/header/filter/algos/Blur.h
+++ Hipe/header/filter/algos/Blur.h
@@ -18,17 +18,17 @@ namespace filter
 			REGISTER(Blur, ()), _connexData(data::INDATA)
 			{
 
-				kernelsize = 3;
+				kernelSize = 3;
 
 			}
 
-			REGISTER_P(int, kernelsize);
+			REGISTER_P(int, kernelSize);
 
 		public:
 			HipeStatus process() override;
 
 		};
 
-		ADD_CLASS(Blur, kernelsize);
+		ADD_CLASS(Blur, kernelSize);
 	}
 }
