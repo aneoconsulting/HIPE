7474201398882287dd237b2c8606bcbb8e1b6146
tmirre <tmirre@aneo.fr>
2017-11-29 17:06:44 +0100

    #72 Updated HOGLiveDetector filter to work in an asynchronous way

--- Hipe/header/filter/algos/HOGLiveDetector.h
+++ Hipe/header/filter/algos/HOGLiveDetector.h
@@ -18,79 +18,43 @@ namespace filter
 			CONNECTOR(data::Data, data::ShapeData);
 			REGISTER(HOGLiveDetector, ()), _connexData(data::INDATA)
 			{
+				_isThreadRunning = true;
+				_pFilterThread = nullptr;
+				_currentInputFrame = nullptr;
+
+				skip_frames = 4;
+
+				startFilterThread();
 			}
 
-			REGISTER_P(char, unused);
+			REGISTER_P(int, skip_frames);
+
+			std::atomic<bool> _isThreadRunning;										//<! boolean to know if the main thread of the filter is already running
+			boost::thread* _pFilterThread;											//<! pointer to the filter's asynchronous thread
+
+			core::queue::ConcurrentQueue<data::DlibDetectorData> _inputDataStack;	//<! [TODO] The queue containing the frames to process.
+			core::queue::ConcurrentQueue<data::ShapeData> _outputDataStack;			//<! [TODO] The queue containing the results of all the detections to process
+
+			data::ImageData* _currentInputFrame;
+			data::ShapeData _currentOutputData;
 
 		public:
-			HipeStatus process() override
-			{
-				// Assert input data count is correct
-				if (_connexData.size() != 2)
-				{
-					std::stringstream errorMessage;
-					errorMessage << "Error in HOGLiveDetector: wrong input data count. Expected 2 (detectors and image), received " << _connexData.size() << "." << std::endl;
-					throw HipeException(errorMessage.str());
-				}
-
-				// Pop and cast data
-				data::Data data1 = _connexData.pop();
-				data::Data data2 = _connexData.pop();
-
-				data::ImageData* image;
-				data::DlibDetectorData* detectors;
-
-				if (data1.getType() == data::IODataType::IMGF)
-				{
-					image = static_cast<data::ImageData *>(&data1);
-					detectors = static_cast<data::DlibDetectorData *>(&data2);
-				}
-				else
-				{
-					image = static_cast<data::ImageData *>(&data2);
-					detectors = static_cast<data::DlibDetectorData *>(&data1);
-				}
-
-				// Assert data types are correct
-				if (image->getType() != data::IODataType::IMGF || detectors->getType() != data::IODataType::DLIBDTCT)
-					throw HipeException("Error in HogLiveDetector: wrong input data types");
-
-				// Detect objects in image
-				std::vector<cv::Rect> detectedObjects = hogDetect(image->getMat(), detectors->detectors_const());
-
-				// Output ShapeData
-				data::ShapeData output;
-				output << detectedObjects;
-
-				_connexData.push(output);
-				return OK;
-			}
+			HipeStatus process() override;
+			void dispose() override;
 
 		private:
-			static inline cv::Rect dlibToCVRect(const dlib::rectangle& r)
-			{
-				return cv::Rect(cv::Point2i(r.left(), r.top()), cv::Point2i(r.right() + 1, r.bottom() + 1));
-			};
+			static inline cv::Rect dlibToCVRect(const dlib::rectangle& r);
+			std::vector<cv::Rect> hogDetect(const cv::Mat& frame, const std::vector<dlib::object_detector<data::hog_trainer::image_scanner_type> >& detectors) const;
 
-			std::vector<cv::Rect> hogDetect(const cv::Mat& frame, const std::vector<dlib::object_detector<data::hog_trainer::image_scanner_type> >& detectors) const
-			{
-				// Convert the OpenCV image to a dlib image.
-				dlib::array2d<dlib::bgr_pixel> dlib_image;
-				dlib::assign_image(dlib_image, dlib::cv_image<dlib::bgr_pixel>(frame));
 
-				// Evaluate the detectors.
-				std::vector<dlib::rectangle> dlib_rects = dlib::evaluate_detectors(detectors, dlib_image);
+		private:
+			void startFilterThread();
 
-				std::vector<cv::Rect> rects;
-				for (auto dlib_rect : dlib_rects)
-				{
-					rects.push_back(dlibToCVRect(dlib_rect));
-				}
-
-				return rects;
-			}
+			data::ShapeData detect(const std::vector<dlib::object_detector<data::hog_trainer::image_scanner_type> >& detectors);
+			void pushInputDetector(const data::DlibDetectorData& detector);
+			data::ShapeData popOutputData();
 		};
 
-		ADD_CLASS(HOGLiveDetector, unused);
+		ADD_CLASS(HOGLiveDetector, skip_frames);
 	}
 }
---
+++ Hipe/source/filter/algos/HOGLiveDetector.cpp
@@ -0,0 +1,130 @@
+#include <filter/algos/HOGLiveDetector.h>
+
+HipeStatus filter::algos::HOGLiveDetector::process()
+{
+	{
+		// Assert input data count is correct
+		if (_connexData.size() != 2)
+		{
+			std::stringstream errorMessage;
+			errorMessage << "Error in HOGLiveDetector: wrong input data count. Expected 2 (detectors and image), received " << _connexData.size() << "." << std::endl;
+			throw HipeException(errorMessage.str());
+		}
+
+		// Pop and cast data
+		data::Data data1 = _connexData.pop();
+		data::Data data2 = _connexData.pop();
+
+		data::ImageData* image;
+		data::DlibDetectorData* detectors;
+
+		if (data1.getType() == data::IODataType::IMGF)
+		{
+			image = static_cast<data::ImageData *>(&data1);
+			detectors = static_cast<data::DlibDetectorData *>(&data2);
+		}
+		else
+		{
+			image = static_cast<data::ImageData *>(&data2);
+			detectors = static_cast<data::DlibDetectorData *>(&data1);
+		}
+
+		// Assert data types are correct
+		if (image->getType() != data::IODataType::IMGF || detectors->getType() != data::IODataType::DLIBDTCT)
+			throw HipeException("Error in HogLiveDetector: wrong input data types");
+
+		// Populate local fields to allow detect method in thread to run
+		_currentInputFrame = image;
+		auto detectorsVec = detectors->detectors_const();
+		data::DlibDetectorData detectorsCopy(detectorsVec);
+		pushInputDetector(detectorsCopy);
+
+		data::ShapeData output = popOutputData();
+		_connexData.push(output);
+		_currentInputFrame = nullptr;
+		return OK;
+	}
+}
+
+void filter::algos::HOGLiveDetector::dispose()
+{
+	_isThreadRunning = false;
+
+	_currentInputFrame = nullptr;
+
+	if (_pFilterThread != nullptr)
+	{
+		_pFilterThread->join();
+		delete _pFilterThread;
+		_pFilterThread = nullptr;
+	}
+}
+
+cv::Rect filter::algos::HOGLiveDetector::dlibToCVRect(const dlib::rectangle& r)
+{
+	return cv::Rect(cv::Point2i(r.left(), r.top()), cv::Point2i(r.right() + 1, r.bottom() + 1));
+}
+
+std::vector<cv::Rect> filter::algos::HOGLiveDetector::hogDetect(const cv::Mat& frame, const std::vector<dlib::object_detector<data::hog_trainer::image_scanner_type>>& detectors) const
+{
+	// Convert the OpenCV image to a dlib image.
+	dlib::array2d<dlib::bgr_pixel> dlib_image;
+	dlib::assign_image(dlib_image, dlib::cv_image<dlib::bgr_pixel>(frame));
+
+	// Evaluate the detectors.
+	std::vector<dlib::rectangle> dlib_rects = dlib::evaluate_detectors(detectors, dlib_image);
+
+	std::vector<cv::Rect> rects;
+	for (auto dlib_rect : dlib_rects)
+	{
+		rects.push_back(dlibToCVRect(dlib_rect));
+	}
+
+	return rects;
+}
+
+void filter::algos::HOGLiveDetector::startFilterThread()
+{
+	HOGLiveDetector* pThis = this;
+	_pFilterThread = new boost::thread([pThis]
+	{
+		while (pThis->_isThreadRunning)
+		{
+			data::DlibDetectorData data;
+			if (!pThis->_inputDataStack.trypop_until(data, 30))		// Try to pop during 30s
+				continue;
+
+			data::ShapeData output = pThis->detect(data.detectors_const());
+
+			if (pThis->_outputDataStack.size() != 0)
+				pThis->_outputDataStack.clear();
+
+			pThis->_outputDataStack.push(output);
+		}
+	});
+}
+
+data::ShapeData filter::algos::HOGLiveDetector::detect(const std::vector<dlib::object_detector<data::hog_trainer::image_scanner_type> >& detectors)
+{
+	data::ShapeData output;
+	if (_currentInputFrame == nullptr || detectors.empty())
+		return output;
+
+	if (_currentInputFrame->getMat().data)
+		output << hogDetect(_currentInputFrame->getMat(), detectors);
+
+	return output;
+}
+
+void filter::algos::HOGLiveDetector::pushInputDetector(const data::DlibDetectorData& detector)
+{
+	_inputDataStack.push(detector);
+}
+
+data::ShapeData filter::algos::HOGLiveDetector::popOutputData()
+{
+	const int waitTime = 30;
+	_outputDataStack.trypop_until(_currentOutputData, waitTime);
+
+	return _currentOutputData;
+}
