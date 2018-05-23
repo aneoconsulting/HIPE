4710a8bc3054584baa81487a8bad1a0e6ba52434
ddubuc <ddubuc@aneo.fr>
2018-05-23 18:57:49 +0200

    #104 Add Full Compilation script for linux

---
+++ compile-ALL.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+
+# Get the absolute path to this script with all symlinks resolved.
+THIS_SCRIPT="$(readlink -f "$0")"
+# Get the directory name of this script.
+DIR_SCRIPT="${THIS_SCRIPT%/*}"
+# Change into this script's directory.
+cd -- "$DIR_SCRIPT"
+
+cd source-core
+bash compile.sh
+
+cd source-modules
+bash compile.sh
