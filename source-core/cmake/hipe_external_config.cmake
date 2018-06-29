ad53bc9288a0368471b6cdf6d07f92db3461d350
ddubuc <ddubuc@aneo.fr>
2018-06-29 17:00:27 +0200

    Fix caps issue for scriptHipe.sh

--- source-core/cmake/hipe_external_config.cmake
+++ source-core/cmake/hipe_external_config.cmake
@@ -19,25 +19,30 @@ if(NOT DEFINED HIPE_EXTERNAL)
   endif(DEFINED ENV{HIPE_EXTERNAL})
 endif(NOT DEFINED HIPE_EXTERNAL)
 
-# Make sure that it is an absolute path.
-get_filename_component(HIPE_EXTERNAL "${HIPE_EXTERNAL}" ABSOLUTE)
 
 # Inform the user which value was used.
 message(STATUS "HIPE_EXTERNAL : ${HIPE_EXTERNAL}")
-
-if (NOT (${USE_PREBUILT_DEPENDENCIES} OR ${FORCE_USE_PREBUILT_DEPENDENCIES}) )
-	# Make sure HIPE_EXTERNAL exists.
-	if(NOT EXISTS "${HIPE_EXTERNAL}")
-	  message(FATAL_ERROR "${HIPE_EXTERNAL} does not exist")
-	elseif(NOT IS_DIRECTORY "${HIPE_EXTERNAL}")
-	  message(FATAL_ERROR "${HIPE_EXTERNAL} is not a directory")
-	endif(NOT EXISTS "${HIPE_EXTERNAL}")
-else()
 if(UNIX)
+
+	if(NOT EXISTS "${HIPE_EXTERNAL}")
 		file(MAKE_DIRECTORY "${HIPE_EXTERNAL}")
 		file(MAKE_DIRECTORY "${HIPE_EXTERNAL}/linux64/install")
 	endif()
+else()
+
+
+	if (NOT (${USE_PREBUILT_DEPENDENCIES} OR ${FORCE_USE_PREBUILT_DEPENDENCIES}) )
+	   # Make sure HIPE_EXTERNAL exists.
+	   if(NOT EXISTS "${HIPE_EXTERNAL}")
+	   	  message(FATAL_ERROR "${HIPE_EXTERNAL} does not exist")
+           elseif(NOT IS_DIRECTORY "${HIPE_EXTERNAL}")
+	   	  message(FATAL_ERROR "${HIPE_EXTERNAL} is not a directory")
+ 	   endif(NOT EXISTS "${HIPE_EXTERNAL}")
+	endif()
 endif()
+
+# Make sure that it is an absolute path.
+get_filename_component(HIPE_EXTERNAL "${HIPE_EXTERNAL}" ABSOLUTE)
 
 
 
